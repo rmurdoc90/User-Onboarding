422e93ed7d6d410344e9287d4f8d2c955261339f
Robert Murdoch <rmurdoc90@gmail.com>
2020-03-25 12:26:37 -0700

    12:25PM 3/25 reacted react app , added axios and yup, made Form.js

---
+++ user-onboard/src/component/Form.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import './App.css';
+
+export default function Form(){
+
+    const [formState, setFormState] = useState =({
+        name: '',
+        email:'',
+        password:'',
+        terms:'',
+        
+    })
+}
