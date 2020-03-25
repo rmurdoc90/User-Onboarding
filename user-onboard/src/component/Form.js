5393fdea0e076ebe4a7883080a9e08abf95bca36
Robert Murdoch <rmurdoc90@gmail.com>
2020-03-25 12:43:20 -0700

    12:42PM generic form structure implemented, use state established for inputs from forms

--- user-onboard/src/component/Form.js
+++ user-onboard/src/component/Form.js
@@ -1,13 +1,54 @@
 import React, {useState} from 'react';
-import './App.css';
 
 export default function Form(){
 
-    const [formState, setFormState] = useState =({
+    const [formState, setFormState] = useState({
         name: '',
         email:'',
         password:'',
         terms:'',
-        
+
     })
+
+    return(
+        <form>
+            <label htmlFor='name'>
+                Name
+                <input
+                id='name'
+                type='text'
+                name='name'
+                value={formState.name}
+                />
+            </label> <br/>
+
+            <label htmlFor='email'>
+                Email
+                <input
+                id='email'
+                type='text'
+                name='email'
+                value={formState.email}
+                />
+            </label> <br/>
+
+            <label htmlFor='password'>
+                Password
+                <input
+                id='password'
+                type='text'
+                name='password'
+                value={formState.password}
+                />
+            </label> <br/>
+
+            <label htmlFor='terms' className='terms'>
+                <input
+                type='checkbox'
+                name='terms'
+                checked={formState.terms}
+                />
+            </label> <br/>
+        </form>
+    )
 }
