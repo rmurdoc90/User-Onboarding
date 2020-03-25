6e3c46f0485efaebfa0c37fcdb3f57f400241b4c
Robert Murdoch <rmurdoc90@gmail.com>
2020-03-25 13:43:27 -0700

    form submit, form change and post api completed, working on last part of project 1:43PM

--- user-onboard/src/component/Form.js
+++ user-onboard/src/component/Form.js
@@ -1,4 +1,20 @@
 import React, {useState} from 'react';
+import './Form.css'
+import * as yup from 'yup';
+import axios from 'axios';
+
+
+
+const formSchema = yup.object().shape({
+    name: yup.string().required('Name Required'),
+    email: yup.string().email().required('Invalid Email'),
+    password: yup.string().required('Enter Valid Password'),
+    terms: yup.boolean().oneOf([true], "Please Join The Dark Side")
+})
+
+
+
+
 
 export default function Form(){
 
@@ -10,8 +26,69 @@ export default function Form(){
 
     })
 
+    const [errors, setErrors] = useState({
+        name: '',
+        email:'',
+        password:'',
+        terms:'',
+
+    })
+
+    const [users, setUsers] =useState([])
+
+    const [post, setPost] =useState([])
+
+    const validateChange = e =>{
+        yup
+        .reach(formSchema, e.target.name)
+        .validate(e.target.value)
+        .then(valid =>{
+            setErrors({
+                ...errors, [e.target.name]: ''
+            })
+        })
+        .catch(err =>{
+            setErrors({
+                ...errors,[e.target.name]: err.errors
+            })
+        })
+    }
+
+    const formSubmit = e =>{
+        e.preventDefault();
+        axios
+        .post('https://reqres.in/api/users', formState)
+        .then(res =>{
+            setPost(res.data);
+            console.log("success", post);
+            
+            setFormState({
+                name:'',
+                email:'',
+                password:'',
+                terms:'',
+            })
+            setUsers({
+                ...users, setFormState
+            })
+        })
+        .catch(err =>{
+            console.log(err.res)
+        })
+    }
+
+    const inputChange = e => {
+        e.persist();
+        const newFormData ={
+            ...formState,[e.target.name]:
+            e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        }
+        validateChange(e);
+        setFormState(newFormData)
+    }
+
     return(
-        <form>
+        <form onSubmit={formSubmit}>
             <label htmlFor='name'>
                 Name
                 <input
@@ -19,9 +96,11 @@ export default function Form(){
                 type='text'
                 name='name'
                 value={formState.name}
+                onChange={inputChange}
                 />
+                {errors.name.length > 0 ? (<p className='error'>{errors.name}</p>) : null}
             </label> <br/>
-
+    
             <label htmlFor='email'>
                 Email
                 <input
@@ -29,7 +108,10 @@ export default function Form(){
                 type='text'
                 name='email'
                 value={formState.email}
+                onChange={inputChange}
                 />
+                {errors.email.length > 0 ? (<p className='error'>{errors.email}</p>) : null}
+
             </label> <br/>
 
             <label htmlFor='password'>
@@ -39,7 +121,10 @@ export default function Form(){
                 type='text'
                 name='password'
                 value={formState.password}
+                onChange={inputChange}
                 />
+                {errors.password.length > 0 ? (<p className='error'>{errors.password}</p>) : null}
+
             </label> <br/>
 
             <label htmlFor='terms' className='terms'>
@@ -47,8 +132,14 @@ export default function Form(){
                 type='checkbox'
                 name='terms'
                 checked={formState.terms}
+                onChange={inputChange}
                 />
+                Lover of tacos
+
             </label> <br/>
+
+            <pre>{JSON.stringify(post)}</pre>
+            <button htmlFor='submit button'>Submit</button>
         </form>
     )
 }
